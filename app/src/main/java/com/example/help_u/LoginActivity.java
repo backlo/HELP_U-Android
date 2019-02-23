7c775609feb0582fcd6e914e50065e0e9d41049a
이인권 <hjun4512@gmail.com>
2019-02-24 00:41:13 +0900

    다시 커밋

--- app/src/main/java/com/example/help_u/LoginActivity.java
+++ app/src/main/java/com/example/help_u/LoginActivity.java
@@ -21,6 +21,7 @@ import com.example.help_u.Provider.Data.ServerResponse;
 import com.example.help_u.Provider.Data.UserInfo;
 import com.example.help_u.Provider.ProviderMainActivity;
 import com.example.help_u.Provider.Util.Retrofit.RetrofitService;
+import com.example.help_u.Requester.Activity.RequestMainActivity;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
